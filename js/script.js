af9bb556a48b7699aa3984b066aa2c26555b2f6b
lapivoinerouge <wioladys@gmail.com>
2022-08-06 23:39:58 +0200

    Add game result message

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// computer move
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
@@ -14,6 +15,7 @@ if (randomNumber == 1) {
 
 printMessage('Mój ruch to: ' + computerMove);
 
+// player move
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
@@ -29,3 +31,32 @@ if (playerInput == '1') {
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+// result
+if (playerMove == computerMove) {
+    printMessage('Remis!');
+} else if (playerMove != 'nieznany ruch') {
+    let userWon;
+    if (playerMove == 'kamień') {
+        if (computerMove == 'papier') {
+            userWon = false;
+        } else {
+            userWon = true;
+        }
+    } else if (playerMove == 'papier') {
+        if (computerMove == 'kamień') {
+            userWon = true;
+        } else {
+            userWon = false;
+        }
+    } else {
+        if (computerMove == 'papier') {
+            userWon = true;
+        } else {
+            userWon = false;
+        }
+    }
+    printMessage(userWon ? 'Wygrałeś!' : 'Przegrałeś');
+} else {
+    printMessage('Ruch ' + playerInput + ' nie istnieje. Dozwolone ruchy to: 1, 2, 3. Spróbuj jeszcze raz.')
+}
