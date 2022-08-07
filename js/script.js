997ea298aedd79f1dd02f2c652f6d241388c4158
lapivoinerouge <wioladys@gmail.com>
2022-08-07 18:27:14 +0200

    Add board, images and display choice as image

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
+let playerPoints = 0;
+let computerPoints = 0;
+
 function playGame(playerInput) {
     clearMessages();
+    clearBoard();
 
     function getMoveName(argMoveId) {
         if (argMoveId == 1) {
-            return 'kamień';
+            return 'rock';
         } else if (argMoveId == 2) {
-            return 'papier';
+            return 'paper';
         } else if (argMoveId == 3) {
-            return 'nożyce';
+            return 'scissors';
         }
         printMessage('Nie znam ruchu o id ' + argMoveId + '.');
         return 'nieznany ruch';
@@ -18,28 +22,37 @@ function playGame(playerInput) {
     
         if (playerMove == computerMove) {
             printMessage('Remis!');
+            playerPoints++;
+            computerPoints++;
         } else if (playerMove != 'nieznany ruch') {
             let userWon;
-            if (playerMove == 'kamień') {
-                if (computerMove == 'papier') {
+            if (playerMove == 'rock') {
+                if (computerMove == 'paper') {
                     userWon = false;
                 } else {
                     userWon = true;
                 }
-            } else if (playerMove == 'papier') {
-                if (computerMove == 'kamień') {
+            } else if (playerMove == 'paper') {
+                if (computerMove == 'rock') {
                     userWon = true;
                 } else {
                     userWon = false;
                 }
             } else {
-                if (computerMove == 'papier') {
+                if (computerMove == 'paper') {
                     userWon = true;
                 } else {
                     userWon = false;
                 }
             }
-            printMessage(userWon ? 'Wygrałeś!' : 'Przegrałeś!');
+
+            if (userWon) {
+                playerPoints++;
+                printMessage('Wygrałeś!');
+            } else {
+                computerPoints++;
+                printMessage('Przegrałeś!');
+            }
         } else {
             printMessage('Ruch ' + playerInput + ' nie istnieje. Dozwolone ruchy to: 1, 2, 3. Spróbuj jeszcze raz.')
         }
@@ -55,9 +68,33 @@ function playGame(playerInput) {
     let playerMove = getMoveName(playerInput);
     
     // result
+    showChoice(computerMove, playerMove);
     displayResult(computerMove, playerMove);
+    updateScore();
 }
+
+function updateScore() {
+    let playerScoreDiv = document.getElementById('player-score');
+    let computerScoreDiv = document.getElementById('computer-score');
+
+    playerScoreDiv.innerHTML = playerPoints;
+    computerScoreDiv.innerHTML = computerPoints;
+}
+
+function showChoice(computerMove, playerMove) {
+    document.getElementById('computer-choice-' + computerMove).style.display = 'block';
+    document.getElementById('player-choice-' + playerMove).style.display = 'block';
+}
+
+function clearBoard() {
+    const hands = document.querySelectorAll('[id*=-choice-]');
+    
+    hands.forEach(hand => hand.style.display = 'none');
+}
+
+updateScore();
 
 document.getElementById('play-rock').addEventListener('click', () => playGame('1'));
 document.getElementById('play-paper').addEventListener('click', () => playGame('2'));
 document.getElementById('play-scissors').addEventListener('click', () => playGame('3'));
+
